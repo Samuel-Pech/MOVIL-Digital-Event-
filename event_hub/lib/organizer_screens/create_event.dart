5b9440d78cb17e1c54e152b651df130e6d47061f
Teolpz <teodosi92@gmail.com>
2024-07-15 00:29:27 -0600

    Add post img

--- event_hub/lib/organizer_screens/create_event.dart
+++ event_hub/lib/organizer_screens/create_event.dart
@@ -1,130 +1,289 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class CreateEvent extends StatelessWidget {
+class CreateEvent extends StatefulWidget {
+  @override
+  _CreateEventState createState() => _CreateEventState();
+}
+
+class _CreateEventState extends State<CreateEvent> {
+  final _formKey = GlobalKey<FormState>();
+  final Map<String, dynamic> _formData = {};
+
+  final TextEditingController _fechaInicioController = TextEditingController();
+  final TextEditingController _fechaTerminoController = TextEditingController();
+  final TextEditingController _horaController = TextEditingController();
+
+  Future<void> _submitForm() async {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      final response = await http.post(
+        Uri.parse('http://localhost:4000/api/event/post/img'),
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode(_formData),
+      );
+
+      if (response.statusCode == 201) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Evento registrado exitosamente')),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Error al registrar el evento')),
+        );
+      }
+    }
+  }
+
+  Future<void> _selectDate(BuildContext context, TextEditingController controller) async {
+    DateTime? selectedDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2020),
+      lastDate: DateTime(2100),
+    );
+    if (selectedDate != null) {
+      controller.text = selectedDate.toIso8601String().split('T').first;
+    }
+  }
+
+  Future<void> _selectTime(BuildContext context, TextEditingController controller) async {
+    TimeOfDay? selectedTime = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+    );
+    if (selectedTime != null) {
+      controller.text = selectedTime.format(context);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('Registrar Evento'),
+        backgroundColor: Color(0xFF6D3089),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: SingleChildScrollView( // Añadido
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              SizedBox(height: 15),
-              Center( 
-                child: Text(
-                  'Registrar datos del evento:',
-                  style: TextStyle(
-                    fontSize: 23,
-                    fontWeight: FontWeight.bold,
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height: 15),
+                Center(
+                  child: Text(
+                    'Registrar datos del evento:',
+                    style: TextStyle(
+                      fontSize: 23,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-
-              SizedBox(height: 32),
-              Text(
-                'Nombre del evento:',
-                style: TextStyle(fontSize: 16),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.purple[50],
-                  border: OutlineInputBorder(),
-                ),
-              ),
-
-              SizedBox(height: 16),
-              Text(
-                'Descripción:',
-                style: TextStyle(fontSize: 16),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.purple[50],
-                  border: OutlineInputBorder(),
-                ),
-              ),
-
-              SizedBox(height: 16),
-              Text(
-                'Fecha del evento:',
-                style: TextStyle(fontSize: 16),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.purple[50],
-                  border: OutlineInputBorder(),
-                ),
-              ),
-
-              SizedBox(height: 16),
-              Text(
-                'URL de la imágen:',
-                style: TextStyle(fontSize: 16),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.purple[50],
-                  border: OutlineInputBorder(),
-                ),
-              ),
-
-              SizedBox(height: 16),
-              Text(
-                'Hora de inicio:',
-                style: TextStyle(fontSize: 16),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.purple[50],
-                  border: OutlineInputBorder(),
-                ),
-              ),
-
-              SizedBox(height: 16),
-              Text(
-                'Hora de término:',
-                style: TextStyle(fontSize: 16),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Colors.purple[50],
-                  border: OutlineInputBorder(),
-                ),
-              ),
-
-              SizedBox(height: 32),
-              Center(
-                child: SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(0xFF6D3089), // Background color
-                      padding: EdgeInsets.symmetric(vertical: 20), // Padding inside the button
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10), // Rounded corners
+                SizedBox(height: 32),
+                _buildTextField('Nombre del evento', 'nombre', TextInputType.text),
+                _buildDateField('Fecha de inicio', 'fecha_inicio', _fechaInicioController),
+                _buildDateField('Fecha de término', 'fecha_termino', _fechaTerminoController),
+                _buildTextField('Hora', 'hora', TextInputType.text),
+                _buildDropdownField('ID del tipo de evento', 'tipo_evento_id', [1, 2]),
+                _buildDropdownField('ID del organizador', 'organizador_id', [1, 2]),
+                _buildDropdownField('ID de la categoría', 'categoria_id', [1, 2]),
+                _buildTextField('Ubicación', 'ubicacion', TextInputType.text),
+                _buildTextField('Máximo de personas', 'max_per', TextInputType.number),
+                _buildTextField('Estado', 'estado', TextInputType.text),
+                _buildDropdownField('Autorizado por', 'autorizado_por', [1, 2]),
+                _buildDateField('Fecha de autorización', 'fecha_autorizacion', TextEditingController()),
+                _buildDropdownField('ID de validación', 'validacion_id', [1, 2]),
+                _buildTextField('URL img', 'imagenes', TextInputType.text),
+                SizedBox(height: 32),
+                Center(
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFF6D3089),
+                        padding: EdgeInsets.symmetric(vertical: 20),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      onPressed: _submitForm,
+                      child: Text(
+                        'Registrar este evento',
+                        style: TextStyle(fontSize: 18, color: Colors.white),
                       ),
                     ),
-                    onPressed: () {
-                      // Define what happens when the button is pressed
-                    },
-                    child: Text(
-                      'Registrar este evento',
-                      style: TextStyle(fontSize: 18, color: Colors.white),
-                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
     );
   }
+
+  Widget _buildTextField(String label, String key, TextInputType keyboardType) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            label,
+            style: TextStyle(fontSize: 16),
+          ),
+          TextFormField(
+            keyboardType: keyboardType,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Por favor, ingrese $label';
+              }
+              return null;
+            },
+            onSaved: (value) {
+              if (keyboardType == TextInputType.number) {
+                _formData[key] = int.tryParse(value!) ?? 0;
+              } else {
+                _formData[key] = value!;
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDateField(String label, String key, TextEditingController controller) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            label,
+            style: TextStyle(fontSize: 16),
+          ),
+          TextFormField(
+            controller: controller,
+            readOnly: true,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+            ),
+            onTap: () => _selectDate(context, controller),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Por favor, seleccione $label';
+              }
+              return null;
+            },
+            onSaved: (value) {
+              _formData[key] = value!;
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+   Widget _buildTimeField(String label, String key, TextEditingController controller) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            label,
+            style: TextStyle(fontSize: 16),
+          ),
+          TextFormField(
+            controller: controller,
+            readOnly: true,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+            ),
+            onTap: () => _selectTime(context, controller),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Por favor, seleccione $label';
+              }
+              return null;
+            },
+            onSaved: (value) {
+              _formData[key] = value!;
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDropdownField(String label, String key, List<int> items) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            label,
+            style: TextStyle(fontSize: 16),
+          ),
+          DropdownButtonFormField<int>(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+            ),
+            items: items.map((int value ) {
+              return DropdownMenuItem<int>(
+                value: value,
+                child: Text(value.toString()),
+              );
+            }).toList(),
+            validator: (value) {
+              if (value == null) {
+                return 'Por favor, seleccione $label';
+              }
+              return null;
+            },
+            onChanged: (value) {
+              setState(() {
+                _formData[key] = value!;
+              });
+            },
+            onSaved: (value) {
+              _formData[key] = value!;
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: CreateEvent(),
+  ));
 }
--- event_hub/lib/organizer_screens/list_events.dart
+++ event_hub/lib/organizer_screens/list_events.dart
@@ -1,7 +1,40 @@
 import 'package:flutter/material.dart';
-import 'detail_event.dart'; // Importa el nuevo archivo
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'detail_event.dart';
 
-class ListEvents extends StatelessWidget {
+class ListEvents extends StatefulWidget {
+  @override
+  _ListEventsState createState() => _ListEventsState();
+}
+
+class _ListEventsState extends State<ListEvents> {
+  List<dynamic> events = [];
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchEvents();
+  }
+
+  Future<void> fetchEvents() async {
+    final response = await http.get(Uri.parse('http://localhost:4000/api/event/get/img'));
+    
+    if (response.statusCode == 200) {
+      setState(() {
+        events = json.decode(response.body);
+        isLoading = false;
+      });
+    } else {
+      // Handle the error
+      setState(() {
+        isLoading = false;
+      });
+      throw Exception('Failed to load events');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,88 +59,91 @@ class ListEvents extends StatelessWidget {
                   ),
                 ),
               ),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: 6, // Número de eventos en la lista
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => DetailEvent()),
-                        );
-                      },
-                      child: Card(
-                        margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        child: Column(
-                          children: <Widget>[
-                            Container(
-                              padding: EdgeInsets.all(8.0),
-                              decoration: BoxDecoration(
-                                color: Colors.blue[100],
-                                borderRadius: BorderRadius.vertical(
-                                  top: Radius.circular(12),
-                                ),
+              isLoading
+                  ? Center(child: CircularProgressIndicator())
+                  : Expanded(
+                      child: ListView.builder(
+                        itemCount: events.length,
+                        itemBuilder: (context, index) {
+                          final event = events[index];
+                          return GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => DetailEvent()),
+                              );
+                            },
+                            child: Card(
+                              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12),
                               ),
-                              child: Center(
-                                child: Text(
-                                  '22 de Junio 2024',
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Row(
+                              child: Column(
                                 children: <Widget>[
-                                  Expanded(
-                                    child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                  Container(
+                                    padding: EdgeInsets.all(8.0),
+                                    decoration: BoxDecoration(
+                                      color: Colors.blue[100],
+                                      borderRadius: BorderRadius.vertical(
+                                        top: Radius.circular(12),
+                                      ),
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        event['fecha_inicio'],
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Row(
                                       children: <Widget>[
-                                        Text(
-                                          'Domingo, 8:00 pm a 10:00 pm',
-                                          style: TextStyle(
-                                            fontSize: 14,
-                                            color: Colors.grey,
+                                        Expanded(
+                                          child: Column(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: <Widget>[
+                                              Text(
+                                                event['hora'],
+                                                style: TextStyle(
+                                                  fontSize: 14,
+                                                  color: Colors.grey,
+                                                ),
+                                              ),
+                                              SizedBox(height: 8.0),
+                                              Text(
+                                                event['nombre'],
+                                                style: TextStyle(
+                                                  fontSize: 18,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                            ],
                                           ),
                                         ),
-                                        SizedBox(height: 8.0),
-                                        Text(
-                                          'Introducción a la Inteligencia Artificial',
-                                          style: TextStyle(
-                                            fontSize: 18,
-                                            fontWeight: FontWeight.bold,
+                                        SizedBox(width: 8.0),
+                                        ClipRRect(
+                                          borderRadius: BorderRadius.circular(8.0),
+                                          child: Image.network(
+                                            event['image_url'],
+                                            height: 60,
+                                            width: 70,
+                                            fit: BoxFit.cover,
                                           ),
                                         ),
                                       ],
                                     ),
                                   ),
-                                  SizedBox(width: 8.0),
-                                  ClipRRect(
-                                    borderRadius: BorderRadius.circular(8.0),
-                                    child: Image.network(
-                                      'https://th.bing.com/th/id/OIP.0ZMWETjNwS1MR6jWld04vQHaEK?w=1300&h=731&rs=1&pid=ImgDetMain', // Reemplaza con la URL de tu imagen
-                                      height: 60,
-                                      width: 70,
-                                      fit: BoxFit.cover,
-                                    ),
-                                  ),
                                 ],
                               ),
                             ),
-                          ],
-                        ),
+                          );
+                        },
                       ),
-                    );
-                  },
-                ),
-              ),
+                    ),
             ],
           ),
         ),
